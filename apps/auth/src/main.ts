d11138bd461e29b958cf0e2028ac662aa8045114
OtavioCollere <otaviosk59@gmail.com>
2026-02-06 14:36:46 -0300

    chore(infra): ajustar bootstrap e containers dos microserviços

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,25 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { Logger } from 'nestjs-pino';
 import { AuthModule } from './auth.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
-  await app.listen(process.env.port ?? 3000);
+  
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+    transformOptions: {
+      enableImplicitConversion: true,
+    },
+  }));
+  app.useLogger(app.get(Logger));
+
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT') ?? 3001;
+  
+  await app.listen(port);
 }
 bootstrap();
--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -1,14 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { ReservationsModule } from './reservations.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
 import { Logger } from 'nestjs-pino';
+import { ReservationsModule } from './reservations.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist : true }))
-  app.useLogger(app.get(Logger))
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+    transformOptions: {
+      enableImplicitConversion: true,
+    },
+  }));
+  app.useLogger(app.get(Logger));
 
-  await app.listen(process.env.port ?? 3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT') ?? 3000;
+  
+  await app.listen(port);
 }
 bootstrap();
