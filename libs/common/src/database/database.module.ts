b7cdb3af9376a5943b985eab6756f9bfbd336f69
OtavioCollere <otaviosk59@gmail.com>
2026-02-06 14:37:01 -0300

    refactor(common): simplificar módulos compartilhados de infraestrutura

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { MongooseModule, type ModelDefinition } from '@nestjs/mongoose';
-import { AppConfigModule } from '../config/config.module';
 
 @Module({
   imports : [MongooseModule.forRootAsync({
-    imports : [AppConfigModule],
     useFactory : (configService : ConfigService) => ({
       uri : configService.get('MONGODB_URI')
     }),
