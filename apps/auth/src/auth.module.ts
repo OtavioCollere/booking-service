6ea20959733d47aa9265c2b1e81afc1d0fdcea32
OtavioCollere <otaviosk59@gmail.com>
2026-02-06 14:37:03 -0300

    refactor(services): ajustar composição de módulos dos microserviços

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,9 +1,33 @@
+import { LoggerModule } from '@app/common';
 import { Module } from '@nestjs/common';
+import { ConfigModule, type ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import Joi from 'joi';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule, 
+    LoggerModule, 
+    ConfigModule.forRoot({
+      isGlobal : true,
+      validationSchema : Joi.object({
+        MONGODB_URI : Joi.string().uri().required(),
+        JWT_SECRET : Joi.string().required(),
+        JWT_EXPIRATION : Joi.string().required(),
+        PORT : Joi.number().default(3001),
+      })
+    }),
+    JwtModule.registerAsync({
+    useFactory : (configService : ConfigService) => ({
+      secret : configService.get('JWT_SECRET'),
+      signOptions : {
+        expiresIn : `${configService.get('JWT_EXPIRATION')}s`
+      }
+    })
+  })],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { ReservationsService } from './reservations.service';
-import { ReservationsController } from './reservations.controller';
 import { DatabaseModule, LoggerModule } from '@app/common';
-import { ReservationsRepository } from './reservations.repository';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 import { ReservationDocument, ReservationSchema } from './models/reservation.schema';
+import { ReservationsController } from './reservations.controller';
+import { ReservationsRepository } from './reservations.repository';
+import { ReservationsService } from './reservations.service';
 
 @Module({
   imports: [
@@ -11,7 +13,16 @@ import { ReservationDocument, ReservationSchema } from './models/reservation.sch
     DatabaseModule.forFeature([
       { name: ReservationDocument.name, schema: ReservationSchema }
     ]),
-    LoggerModule
+    LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal : true,
+      validationSchema : Joi.object({
+        MONGODB_URI : Joi.string().uri().required(),
+        PORT : Joi.number().default(3000),
+        JWT_SECRET : Joi.string().required(),
+        JWT_EXPIRATION : Joi.string().required(),
+      })
+    })
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
